635ce48d727d263a56fc206cb0aec5a362567cba
smh1988-2 <seamhurley1988@gmail.com>
2021-12-16 19:55:21 -0800

    commit tree d8cebb9d884bb7f4910a6eef6035e463d43efced

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
+require "pry"
+
 class Dog
 
+    def intialize id = nil, name, breed
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+        Dog.create_table
+        table_check_sql = "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='dogs';"
+        expect(DB[:conn].execute(table_check_sql)[0]).to eq(['dogs'])
+    end
+
 end
+binding.pry
